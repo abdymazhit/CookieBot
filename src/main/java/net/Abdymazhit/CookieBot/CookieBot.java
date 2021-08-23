1c9905ab135ab6292953dfbd4db362036daf6f99
Islam Abdymazhit <islam.abdymazhit@gmail.com>
2021-08-23 14:31:47 +0300

    Добавлена поддержка JDA

--- src/main/java/net/Abdymazhit/CookieBot/CookieBot.java
+++ src/main/java/net/Abdymazhit/CookieBot/CookieBot.java
@@ -1,8 +1,36 @@
 package net.Abdymazhit.CookieBot;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.utils.Compression;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+
+import javax.security.auth.login.LoginException;
+
+/**
+ * Главный класс, отвечает за инициализацию бота
+ *
+ * @version   23.08.2021
+ * @author    Islam Abdymazhit
+ */
 public class CookieBot {
 
-    public static void main(String[] args) {
+    /** Токен бота */
+    public static String token = "BOT-TOKEN";
 
+    /** Главный объект для работы с Discord API */
+    public static JDA jda;
+
+    /**
+     * Инициализирует бота
+     * @throws LoginException Ошибка входа
+     * @throws InterruptedException Ошибка работы Discord API
+     */
+    public static void main(String[] args) throws LoginException, InterruptedException {
+        JDABuilder builder = JDABuilder.createDefault(token);
+        builder.disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE);
+        builder.setBulkDeleteSplittingEnabled(false);
+        builder.setCompression(Compression.ZLIB);
+        jda = builder.build().awaitReady();
     }
 }
